e02562ff1e0fd11c0a13d6d44662bd774ef62b92
Apple <apple@Apples-MacBook-Pro.local>
2020-10-19 18:18:53 +0700

    Updates

---
+++ precache-manifest.e2437ffa5a83e9369691bc28419e8f35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24605b1f867e443231b1c704b4813130",
+    "url": "/ProjectSendo_KhacDuy/index.html"
+  },
+  {
+    "revision": "c5569e43052fa2d6cb70",
+    "url": "/ProjectSendo_KhacDuy/static/css/main.f119ffc8.chunk.css"
+  },
+  {
+    "revision": "c218b228b89377eb5b1f",
+    "url": "/ProjectSendo_KhacDuy/static/js/2.4dec0faf.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/ProjectSendo_KhacDuy/static/js/2.4dec0faf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5569e43052fa2d6cb70",
+    "url": "/ProjectSendo_KhacDuy/static/js/main.3fd15cfa.chunk.js"
+  },
+  {
+    "revision": "54a47f53671fd9c84296",
+    "url": "/ProjectSendo_KhacDuy/static/js/runtime-main.c7ef5bfb.js"
+  }
+]);
