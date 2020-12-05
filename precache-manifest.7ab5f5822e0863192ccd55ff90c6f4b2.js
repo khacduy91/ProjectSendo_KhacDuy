40596160a9b5e021459093518dd029bb7794b493
Apple <apple@Apples-MacBook-Pro.local>
2020-12-06 01:10:35 +0700

    Updates

--- precache-manifest.7ab5f5822e0863192ccd55ff90c6f4b2.js
+++ precache-manifest.7ab5f5822e0863192ccd55ff90c6f4b2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "60fd8cda33c42640a5b9160db518a90e",
+    "revision": "b3f974095e44d0435a96428c944934c2",
     "url": "/ProjectSendo_KhacDuy/index.html"
   },
   {
-    "revision": "dc0c9b6bedd2849d333d",
+    "revision": "16dd9260d1164bf48aad",
     "url": "/ProjectSendo_KhacDuy/static/css/main.12d16c35.chunk.css"
   },
   {
-    "revision": "f6e72f5397e251e63527",
-    "url": "/ProjectSendo_KhacDuy/static/js/2.51440974.chunk.js"
+    "revision": "3a265c8087bcac76725a",
+    "url": "/ProjectSendo_KhacDuy/static/js/2.6dc85922.chunk.js"
   },
   {
     "revision": "f9e7e68e90600aabdd7d1f759413461d",
-    "url": "/ProjectSendo_KhacDuy/static/js/2.51440974.chunk.js.LICENSE.txt"
+    "url": "/ProjectSendo_KhacDuy/static/js/2.6dc85922.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dc0c9b6bedd2849d333d",
-    "url": "/ProjectSendo_KhacDuy/static/js/main.27fef42b.chunk.js"
+    "revision": "16dd9260d1164bf48aad",
+    "url": "/ProjectSendo_KhacDuy/static/js/main.40fa5fc5.chunk.js"
   },
   {
     "revision": "54a47f53671fd9c84296",
